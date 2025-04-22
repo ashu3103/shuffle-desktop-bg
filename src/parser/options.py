7c16d1347fe8e077079d1e3adacdacb0482649fa
ashu3103 <ash2003sharma@gmail.com>
2025-04-22 18:00:29 +0530

    implement option parser

--- src/parser/options.py
+++ src/parser/options.py
@@ -21,9 +21,13 @@ class OptionParameter:
         self.is_flag = is_flag
         self.is_valid = False
         self.argument_value = None
+        self.result_index = -1
     
     def optionSetValid(self, valid: bool):
         self.is_valid = valid
+
+    def optionGetValid(self):
+        return self.is_valid
     
     def optionIsRequired(self) -> bool:
         return self.is_required
@@ -37,18 +41,22 @@ class OptionParameter:
     def optionGetArgumentValue(self) -> str:
         return self.argument_value
 
+def is_option(a: str) -> bool:
+    if (a.startswith('-') or a.startswith('--')): return True
+    return False
+
 ## Ensure only one instance of this parser class in the project
 @singleton
 class OptionParser:
     __short_name_map = {}
     __long_name_map = {}
 
-    __option_parameter_pool = []
+    __option_parameter_pool: List[OptionParameter] = []
 
     #
     # Initialize a parser with a set of config rules
     #
-    def parser_init(self, options_config):
+    def optionParserInit(self, options_config):
         try:
             for i in range(len(options_config)):
                 assert isinstance(options_config[i], list)
@@ -76,20 +84,55 @@ class OptionParser:
     # Parse the options of a command line, flags have more priority than argument options
     # return the next index after parsing, -1 if unsuccess
     #
-    def parse_options(options: List[str]) -> int:
-        next_index = 0
-        for i, option in enumerate(options):
-            if (option.startswith('-')):
-                
-                pass
-            elif (option.startswith('--')):
-                pass
-            ## base case
-            else:
-                break
-            next_index = i
+    def parseOptions(self, options: List[str]) -> int:
+        try:
+            curr_index = 0
+            while(curr_index < len(options)):
+                option = options[curr_index]
+                if (option.startswith('-')):
+                    assert (len(option)==2)
+                    flag = option[1]
+                    if (self.__short_name_map.__contains__(flag)):
+                        option_parameter: OptionParameter = self.__short_name_map[flag]
+                        if (option_parameter.optionGetValid()):  ## if already a flag was detected ignore it
+                            curr_index = curr_index + 1
+                            continue
+                        if (not option_parameter.optionIsFlag):
+                            curr_index = curr_index + 1
+                            assert (not is_option(options[curr_index]))
+                            option_parameter.optionSetArgumentValue(options[curr_index])
+                        option_parameter.optionSetValid(True)
+                    else:
+                        return -1
+                elif (option.startswith('--')):
+                    flag = option.split('=')[0].split('--')[1]
+                    if (self.__long_name_map.__contains__(flag)):
+                        option_parameter: OptionParameter = self.__long_name_map[flag]
+                        if (option_parameter.optionGetValid()):  ## if already a flag was detected ignore it
+                            curr_index = curr_index + 1
+                            continue
+                        if (not option_parameter.optionIsFlag):
+                            argument = option.split('=')[1]
+                            option_parameter.optionSetArgumentValue(argument)
+                        option_parameter.optionSetValid(True)
+                    else:
+                        return -1
+                ## base case
+                else:
+                    curr_index = curr_index + 1
+                    break
+                curr_index = curr_index + 1
 
-        return next_index
+            for op in self.__option_parameter_pool:
+                if (op.optionIsRequired() and (not op.optionGetValid())):
+                    return -1
+            return curr_index
+        except AssertionError as e:
+            print(f"Assertion failed: {e}")
+            return -1
+    
+    def getOptionParserResult(self):
+        return self.__result
         
 
 
